d8b8e35477cc9d66f7bb06db121aec4e256eb51f
dbelliss <daveybelliss@yahoo.com>
2016-10-01 17:37:43 -0700

    Basic Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,13 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
 	private Rigidbody2D rb;
-
+	private Animator animator; 
+	private SpriteRenderer spriteRenderer;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -18,9 +21,26 @@ public class PlayerMovement : MonoBehaviour {
 
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		if (moveHorizontal == 0) {
+			animator.ResetTrigger ("isMoving");
+			print ("1");
+		} else {
+			animator.SetTrigger ("isMoving");
+			print ("2");
+			if (moveHorizontal < 0) {
+				spriteRenderer.flipX = true;
+				print ("3");
+			}
+		}
+
+		if (moveHorizontal >= 0) {
+			spriteRenderer.flipX = false;
+		}
+	
+
 		rb.velocity = (movement * speed);
 	}
 }
