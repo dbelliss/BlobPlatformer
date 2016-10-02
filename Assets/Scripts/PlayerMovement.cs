2ecfa3b979e63728849b05e2ddf1be73751f3bd9
dbelliss <daveybelliss@yahoo.com>
2016-10-01 18:46:09 -0700

    Basic Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,26 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
+
 	private Rigidbody2D rb;
 	private Animator animator; 
 	private SpriteRenderer spriteRenderer;
+
+	//For horizontal movement
+	bool facingRight = true;
+
+	//For Jumping
+	float groundRadius = .2f;
+	private bool grounded = false;
+	public Transform floorDetector;
+	public LayerMask whatIsGround;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 		animator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
@@ -20,20 +32,20 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle (floorDetector.position, groundRadius, whatIsGround);
+		animator.SetBool ("grounded", grounded);
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
 
+		animator.SetFloat ("speed", Mathf.Abs (moveHorizontal));
 
-		if (moveHorizontal == 0) {
-			animator.ResetTrigger ("isMoving");
-			print ("1");
-		} else {
-			animator.SetTrigger ("isMoving");
-			print ("2");
-			if (moveHorizontal < 0) {
-				spriteRenderer.flipX = true;
-				print ("3");
-			}
+		if (facingRight && moveHorizontal < 0) {
+			flip ();
+			facingRight = !facingRight;
+		}
+		else if (!facingRight && moveHorizontal > 0) {
+			flip();
+			facingRight = !facingRight;
 		}
 
 		if (moveHorizontal >= 0) {
@@ -42,5 +54,11 @@ public class PlayerMovement : MonoBehaviour {
 	
 
 		rb.velocity = (movement * speed);
+	}
+
+	void flip() {
+		Vector3 temp = transform.localScale;
+		temp.x *= -1;
+		transform.localScale = temp;
 	}
 }
