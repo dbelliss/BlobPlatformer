d7c8f2aded685ad3dcf2ebe2ec430e0143559469
Thomas Tran <thomastran878@gmail.com>
2016-11-01 03:28:58 -0700

    Added engaging and tracking sensors, Added zombie ai script, zombie prefab updated

---
+++ Assets/Scripts/TrackingSensor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingSensor : MonoBehaviour {
+    private ZombieAi zombie;
+
+    // Use this for initialization
+    void Start () {
+        zombie = transform.parent.gameObject.GetComponent<ZombieAi>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            zombie.target = null;
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieAi.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAi : MonoBehaviour {
+    public Transform target = null;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            Vector3 targetDirection = target.position - transform.position;
+            float flipValue = transform.localScale.x;
+
+            if (targetDirection.x < 0)
+            {
+                if (flipValue < 0)
+                    flip();
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (flipValue > 0)
+                    flip();
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        target = other.transform;
+    //    }
+    //}
+
+    void flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+    }
+}
