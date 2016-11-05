18256fe6af1f30021d88c26d31f6ada91327d35d
Thomas Tran <thomastran878@gmail.com>
2016-11-05 14:42:22 -0700

    Chest is functional - can be regular, mimic, random

---
+++ Assets/Scripts/ChestInteract.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestInteract : MonoBehaviour {
+    private bool interaction = false;
+    private Animator animator;
+    public bool isRegular;
+    public bool isMimic;
+    public bool isRandom;
+    public float randomChanceMimic;
+
+	// Use this for initialization
+	void Start () {
+        animator = transform.parent.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float verticalDirection = Input.GetAxis("Vertical");
+        if (verticalDirection > 0 && interaction && isRegular)
+        {
+            animator.SetBool("OpenRegular", true);
+        }
+        else if (verticalDirection > 0 && interaction && isMimic)
+        {
+            animator.SetBool("OpenMimic", true);
+        }
+        else if (verticalDirection > 0 && interaction && isRandom)
+        {
+            float randomNum = Random.Range(0.0f, 1.0f);
+            if (randomNum < randomChanceMimic)
+                animator.SetBool("OpenMimic", true);
+            else
+                animator.SetBool("OpenRegular", true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            interaction = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            interaction = false;
+    }
+}
