4248a8083c5706ca44d170057f37826aaaae2ca4
Thomas Tran <thomastran878@gmail.com>
2016-11-19 20:48:43 -0800

    Added attack animation

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Attack"))
+        {
+            animator.SetBool("attack", true);
+        }
+        else
+        {
+            animator.SetBool("attack", false);
+        }
+	}
+}
