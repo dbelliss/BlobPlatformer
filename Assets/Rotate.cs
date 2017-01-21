00e99218b84be78088de063bfb4b7a58eb77f519
dbelliss <daveybelliss@yahoo.com>
2017-01-20 18:15:03 -0800

    Adding rotating pendulum

---
+++ Assets/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	public float torque;
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		rb.AddTorque(transform.forward * torque);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+}
